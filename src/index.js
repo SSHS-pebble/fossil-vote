b52fda5c46f829ff318fd30e452cc1598c37c2ab
Younghun Roh <yhunroh@gmail.com>
2019-06-13 23:49:12 +0900

    Basic votes and designs

--- src/index.js
+++ src/index.js
@@ -1,7 +1,35 @@
-const serve = require('koa-static');
 const Koa = require('koa');
+const views = require('koa-views');
+const router = new require('koa-router')();
+const bodyparser = require('koa-body');
+const fetch = require('node-fetch');
+
 const app = new Koa();
 
-app.use(serve('./static'));
-  
+const SERVER = 'http://localhost:8000/vote';
+
+
+app.use(views(__dirname + '/static', {extension: 'html'}));
+
+app.use(bodyparser());
+
+router.get('/', ctx => {
+    console.log("Rendered a page");
+    return ctx.render('vote');
+});
+router.post('/', ctx => {
+    let obj = ctx.request.body
+    console.log(obj);
+    console.log(JSON.stringify(obj));
+
+    fetch(SERVER, {
+        method: 'POST',
+        body: JSON.stringify(obj),
+    })
+
+    return ctx.render('voted');
+});
+
+app.use(router.routes()).use(router.allowedMethods);
+
 app.listen(3000);
